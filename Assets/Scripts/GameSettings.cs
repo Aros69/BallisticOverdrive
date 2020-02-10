46f87d2b6566048740fe6f8540a1280f92918a33
unknown <a.alizahidi@gmail.com>
2020-02-10 12:05:17 +0100

    Character Controls

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    [Header("Teams parameters")]
+    [SerializeField]
+    public static int redTeamHealth = 5;
+    [SerializeField]
+    public static int blueTeamHealth = 1;
+}
