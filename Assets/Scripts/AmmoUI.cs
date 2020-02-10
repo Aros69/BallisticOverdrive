c7553aa48f0a7776936fbf8ebcf9300e582fe160
PoPPoo <bao-anh.nguyen@etu.univ-lyon1.fr>
2020-02-10 12:08:07 +0100

    UI not done

---
+++ Assets/Scripts/AmmoUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class AmmoUI : MonoBehaviour
+{
+	PlayerStats m_playerStats;
+	int ammo;
+
+	private void Awake()
+	{
+		PlayerAmmoUpdated(ClientScene.localPlayer);
+		PlayerAnnouncer.OnPlayerStatsUpdated += PlayerAmmoUpdated;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (m_playerStats != null)
+		{
+			GetComponent<Text>().text = m_playerStats.Ammo.ToString();
+		}
+	}
+
+	private void OnDestroy()
+	{
+		PlayerAnnouncer.OnPlayerStatsUpdated -= PlayerAmmoUpdated;
+	}
+
+	void PlayerAmmoUpdated(NetworkIdentity localPlayer)
+	{
+		if (localPlayer != null)
+		{
+			m_playerStats = localPlayer.GetComponent<PlayerStats>();
+		}
+
+		this.enabled = (localPlayer != null);
+	}
+}
---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class HealthUI : MonoBehaviour
+{
+	PlayerStats m_playerStats;
+	int m_health;
+
+	private void Awake()
+	{
+		PlayerHealthUpdated(ClientScene.localPlayer);
+		PlayerAnnouncer.OnPlayerStatsUpdated += PlayerHealthUpdated;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		if (m_playerStats != null)
+		{
+			GetComponent<Text>().text = m_playerStats.Health.ToString();
+		}
+	}
+
+	private void OnDestroy()
+	{
+		PlayerAnnouncer.OnPlayerStatsUpdated -= PlayerHealthUpdated;
+	}
+
+	void PlayerHealthUpdated(NetworkIdentity localPlayer)
+	{
+		if (localPlayer != null)
+		{
+			m_playerStats = localPlayer.GetComponent<PlayerStats>();
+		}
+
+		this.enabled = (localPlayer != null);
+	}
+}
