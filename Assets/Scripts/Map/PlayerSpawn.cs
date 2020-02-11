e62a67562115867259f582c732375058d4d113be
florent <florent.dupont.it@gmail.com>
2020-02-11 23:04:03 +0100

    Add HUD manager, some assets and map elements

---
+++ Assets/Scripts/Map/PlayerSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpawnType
+{
+    generic,
+    attacker, 
+    defender
+}
+
+public class PlayerSpawn : MonoBehaviour
+{
+    [SerializeField] private SpawnType spawnType;
+    private void OnDrawGizmos()
+    {
+        if (spawnType == SpawnType.attacker) {
+            Gizmos.color = new Color(1.0f, 0.0f, 0.0f, 0.5f);
+        } else if(spawnType == SpawnType.defender) { 
+            Gizmos.color = new Color(0.0f, 0.0f, 1.0f, 0.5f);
+        } else { 
+            Gizmos.color = new Color(0.0f, 0.0f, 0.0f, 0.5f);
+        }
+
+        Gizmos.DrawCube(transform.position, new Vector3(.6f, 1.8f, .6f));
+    }
+}
