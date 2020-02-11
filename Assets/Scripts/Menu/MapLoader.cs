be98109f0306369238e5497bb66ea487385c4131
florent <florent.dupont.it@gmail.com>
2020-02-11 12:17:18 +0100

    Add connection from the main menu, loading of map

---
+++ Assets/Scripts/Menu/MapLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapLoader : MonoBehaviour
+{
+    [SerializeField] private string m_mapToLoad = "elite_base";
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadMap(m_mapToLoad);
+    }
+    
+    void LoadMap(string name)
+    {
+        SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
+    }
+}
