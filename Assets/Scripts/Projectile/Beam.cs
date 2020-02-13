285aaed81c053ef2d63eb28dee359f6a50f87654
Robin <robin.donnay@etu.univ-lyon1.fr>
2020-02-13 21:41:27 +0100

    Player test for hit scan

--- Assets/Scripts/Projectile/Beam.cs
+++ Assets/Scripts/Projectile/Beam.cs
@@ -1,13 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class Beam : MonoBehaviour
+public class Beam : NetworkBehaviour
 {
+
+    void playSound(){
+        GetComponent<AudioSource>().Play(0);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playSound();
+    }
+
+    public void shoot(){
+        // Launch a hit scan from the middle of the viewport in front of him. 
+        // If the hitscan touch an object, call the hit function of this object
+            // and define the length of the laserbeam based on the distance of the object
+        // Else use a base distance (100 or less... I don't know)
+        // Show for a certain time (less than a sec... 0,5s maybe) the shot base on
+        // the distance and the start shot point
     }
 
     // Update is called once per frame
