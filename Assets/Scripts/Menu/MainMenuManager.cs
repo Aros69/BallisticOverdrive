300627c18439f185eac6c7b261e55f73c513eb39
Florent Dupont <florent.dupont.it@gmail.com>
2020-02-13 13:43:32 +0100

    Add Server List UI + Add host mode to menu + Fix for client and host disconnect

--- Assets/Scripts/Menu/MainMenuManager.cs
+++ Assets/Scripts/Menu/MainMenuManager.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private TMP_InputField serverAddressInputField;
+
+    void Start(){
+        Cursor.visible = true;
+    }
 
     public void ConnectToServer()
     {
         ODNetworkManager.singleton.networkAddress = serverAddressInputField.text;
         ODNetworkManager.singleton.StartClient();
+    }
+
+    public void CreateGame()
+    {
+        ODNetworkManager.singleton.StartHost();
     }
 
     public void Quit(){
