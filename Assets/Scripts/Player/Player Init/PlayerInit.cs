d7132b5b4d17629a7831d7ae57ab3f2d3e2d2da2
Ali <a.alizahidi@gmail.com>
2020-02-19 16:16:07 +0100

    Disabled local player's mesh

--- Assets/Scripts/Player/Player Init/PlayerInit.cs
+++ Assets/Scripts/Player/Player Init/PlayerInit.cs
@@ -5,10 +5,18 @@ using Mirror;
 public class PlayerInit : NetworkBehaviour
 {
     [SerializeField] private GameObject m_camera;
+    [SerializeField] private GameObject m_Mesh;
     void Start()
     {
         if(isLocalPlayer)
+        {
             m_camera.SetActive(true);
+            //Disable the objects storing the red/blue meshes because m_Mesh is used to compute the tilt from the movements)
+            foreach(Transform child in m_Mesh.transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
         else
             m_camera.SetActive(false);
     }   
