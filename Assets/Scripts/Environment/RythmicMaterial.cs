bef2206c9d977e88907008d2275b51d1429592ee
Ali <a.alizahidi@gmail.com>
2020-02-20 16:10:09 +0100

    Material's shader changed + RythmicNeon shader + Elite battleground Neon

--- Assets/Scripts/Environment/RythmicMaterial.cs
+++ Assets/Scripts/Environment/RythmicMaterial.cs
@@ -8,11 +8,15 @@ public class RythmicMaterial : MonoBehaviour
 
     [SerializeField] Parameter m_parameter = Parameter.EmissionIntensity;
     [SerializeField] private int m_frequencyBand = 0;
-    [SerializeField] private float m_scaleFrequencyBand = 100.0f;
+    [SerializeField] private float m_scaleFrequencyBand = 5.0f;
 
     private AudioSpectrum m_audioSpectrum = null;
     private Renderer m_renderer = null;
+    private List<Material> m_materials = null;
+    // private List<float> m_baseParameters = null; // Base parameters found on the material
 
+
+    // public float maxFrequency, minFrequency = 0;
     void Start()
     {
         if (m_frequencyBand > 7)
@@ -22,48 +26,84 @@ public class RythmicMaterial : MonoBehaviour
         if (objs.Length == 0)
             throw new Exception("No AudioSpectrum object found, are you sure it's correctly tagged with \"AudioSpectrum\" ?");
         m_audioSpectrum = objs[0].GetComponent<AudioSpectrum>();
-        m_renderer = GetComponent<Renderer>();
+        m_materials = new List<Material>(GetComponent<Renderer>().materials);
     }
 
     void Update()
     {
         float f = m_audioSpectrum.GetBandWidthValue(m_frequencyBand) * m_scaleFrequencyBand;
+        Debug.Log("Here" + f);
         switch (m_parameter)
         {
             case Parameter.EmissionIntensity:
-                m_renderer.material.SetFloat("Vector1_C7CE0749", f);
+                SetFloatForMaterials("Vector1_C7CE0749", f);
                 break;
             case Parameter.Red:
-                m_renderer.material.SetColor("Color_8C5B6BEE", new Color(f, 1.0f, 1.0f, 1.0f));
+                SetColorForMaterials("Color_8C5B6BEE", new Color(f, 1.0f, 1.0f, 1.0f));
                 break;
             case Parameter.Blue:
-                m_renderer.material.SetColor("Color_8C5B6BEE", new Color(1.0f, f, 1.0f, 1.0f));
+                SetColorForMaterials("Color_8C5B6BEE", new Color(1.0f, f, 1.0f, 1.0f));
                 break;
             case Parameter.Green:
-                m_renderer.material.SetColor("Color_8C5B6BEE", new Color(1.0f, 1.0f, f, 1.0f));
+                SetColorForMaterials("Color_8C5B6BEE", new Color(1.0f, 1.0f, f, 1.0f));
                 break;
             case Parameter.RedEmission:
-                m_renderer.material.SetColor("Color_45AD81E0", new Color(f, 1.0f, 1.0f, 1.0f));
+                SetColorForMaterials("Color_45AD81E0", new Color(f, 1.0f, 1.0f, 1.0f));
                 break;
             case Parameter.BlueEmission:
-                m_renderer.material.SetColor("Color_45AD81E0", new Color(1.0f, f, 1.0f, 1.0f));
+                SetColorForMaterials("Color_45AD81E0", new Color(1.0f, f, 1.0f, 1.0f));
                 break;
             case Parameter.GreenEmission:
-                m_renderer.material.SetColor("Color_45AD81E0", new Color(1.0f, 1.0f, f, 1.0f));
+                SetColorForMaterials("Color_45AD81E0", new Color(1.0f, 1.0f, f, 1.0f));
                 break;
             case Parameter.TilingX:
-                m_renderer.material.SetVector("Vector2_5AB9E672", new Vector2(f, 1));
+                SetVector2DForMaterials("Vector2_5AB9E672", new Vector2(f, 1));
                 break;
             case Parameter.TilingY:
-                m_renderer.material.SetVector("Vector2_5AB9E672", new Vector2(1, f));
+                SetVector2DForMaterials("Vector2_5AB9E672", new Vector2(1, f));
                 break;
             case Parameter.OffsetX:
-                m_renderer.material.SetVector("Vector2_B6029552", new Vector2(f, 1));
+                SetVector2DForMaterials("Vector2_B6029552", new Vector2(f, 1));
                 break;
             case Parameter.OffsetY:
-                m_renderer.material.SetVector("Vector2_B6029552", new Vector2(1, f));
+                SetVector2DForMaterials("Vector2_B6029552", new Vector2(1, f));
                 break;
         }
+    }
 
+    /// <summary>
+    /// Update the max and minimum frequency
+    /// Used to compute the increment 
+    /// </summary>
+    /// <param name="f"></param>
+    // void UpdateFrequencyStats(float f)
+    // {
+    //     if(f > maxFrequency)
+    //         maxFrequency = f;
+    // }
+
+    void SetColorForMaterials(String propertyName, Color col)
+    {
+        foreach(Material mat in m_materials)
+        {
+            if(mat.HasProperty(propertyName))
+                mat.SetColor(propertyName, col);
+        }
+    }
+    void SetFloatForMaterials(String propertyName, float f)
+    {
+        foreach(Material mat in m_materials)
+        {   
+            if(mat.HasProperty(propertyName))
+                mat.SetFloat(propertyName, f);
+        }
+    }
+    void SetVector2DForMaterials(String propertyName, Vector2 v)
+    {
+        foreach(Material mat in m_materials)
+        {   
+            if(mat.HasProperty(propertyName))
+                mat.SetVector(propertyName, v);
+        }
     }
 }
