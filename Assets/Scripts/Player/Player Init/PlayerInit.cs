7f0fbc7595862fb52085518e47698af124ea9eda
Ali <a.alizahidi@gmail.com>
2020-02-24 13:43:20 +0100

    Added Dollify()

--- Assets/Scripts/Player/Player Init/PlayerInit.cs
+++ Assets/Scripts/Player/Player Init/PlayerInit.cs
@@ -8,18 +8,44 @@ public class PlayerInit : NetworkBehaviour
     [SerializeField] private GameObject m_Mesh;
     void Start()
     {
-        if(isLocalPlayer)
+        if (isLocalPlayer)
         {
             m_camera.SetActive(true);
             //Disable the objects storing the red/blue meshes because m_Mesh is used to compute the tilt from the movements)
-            foreach(Transform child in m_Mesh.transform)
+            foreach (Transform child in m_Mesh.transform)
                 child.gameObject.SetActive(false);
         }
         else
         {
-            m_camera.SetActive(false);
-            GetComponent<PlayerController>().enabled = false;
+            disableControls();
             GetComponent<Rigidbody>().isKinematic = true;
         }
+    }
+
+    void disableControls()
+    {
+        m_camera.SetActive(false);
+        GetComponent<PlayerController>().enabled = false;
+    }
+
+    [ClientRpc]
+    void RpcDollify()
+    {
+        disableControls();
+        Rigidbody r = GetComponent<Rigidbody>();
+        r.isKinematic = false;
+        r.freezeRotation = false;
+    }
+
+    [Command]
+    void CmdDollify()
+    {
+        RpcDollify();
+    }
+    //TODO REMOVE
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+            CmdDollify();
     }
 }
